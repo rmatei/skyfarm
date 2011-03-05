47f0dc95df02188ab6cf7c62fcbaf6076a0b2820
Richard Dillman <dillman85@gmail.com>
2011-03-04 17:16:29 -0800

    first version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Skyfarm::Application.config.secret_token = '80eb417a8d0007c6e8de4e59515e560ba64564e832b58fa45343e5e519911e1aa1b4b07d373c0503d61d16a4077433b8a375bbca2fdc92548152a6c739184cd9'
