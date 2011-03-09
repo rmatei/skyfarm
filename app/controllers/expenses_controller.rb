439793d36ec036ce4709d2b54c0545fb83875806
Richard Dillman <dillman85@gmail.com>
2011-03-08 19:12:11 -0800

    Adding Venmo transaction ID to expenses scaffold

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,3 +1,8 @@
 class ExpensesController < ApplicationController
-  active_scaffold :expense
+  
+  active_scaffold :expense do |config|
+    config.columns << :venmo_transaction_id
+    config.columns[:venmo_transaction_id].label = 'Venmo transaction ID'
+  end
+  
 end
