a74e0a781584803a4fde90818237618e75e50bcc
Richard Dillman <dillman85@gmail.com>
2011-03-08 17:09:33 -0800

    starting to parse API response

--- app/controllers/venmo_controller.rb
+++ app/controllers/venmo_controller.rb
@@ -1,8 +1,15 @@
 class VenmoController < ApplicationController
 
+  # takes Venmo API call, creates new expense
   def track_receipt
-    # take params, create new expense
-    # ...
+    # extracts serialized string from parameters
+    data = params.first.last.keys.first
+    
+    # remove malformed parameter that JSON chokes on
+    clean_data = data.gsub(/\"img_url.*?, /, "").gsub(/\"img_url.*?, /, "")
+    
+    data = JSON.parse clean_data
+    logger.info data.inspect
     
     render :text => "ok"
   end
