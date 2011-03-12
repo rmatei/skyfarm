a021ca8f32ac16d1d801872aa8ebfa6d049219f6
Robert Cezar Matei <rmatei@gmail.com>
2011-03-11 22:44:16 -0800

    changing how we display tallied expenses per person

--- app/helpers/expenses_helper.rb
+++ app/helpers/expenses_helper.rb
@@ -6,7 +6,13 @@ module ExpensesHelper
   
   def amount_per_person_column(record)
     formatted_amount = number_to_currency(record.amount_per_person)
-    record.general? ? formatted_amount : "approx. #{formatted_amount}"
+    if record.general? 
+      formatted_amount
+    elsif record.food? 
+      "approx. #{formatted_amount}"
+    else
+      "by tally"
+    end
   end
   
   def created_at_column(record)
