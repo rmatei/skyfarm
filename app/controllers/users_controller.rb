27f5f18e2052114ca7f64f10f49f1f132469299a
Robert Cezar Matei <rmatei@gmail.com>
2011-04-16 17:51:18 -0700

    Polished up billing email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   
   active_scaffold :user do |config|
     # Limited column display for usability
-    config.list.columns = [:profile_picture, :full_name, :venmo_username, :total_spent, :expenses, :monthly_rent, :food_multiplier]
+    config.list.columns = [:profile_picture, :full_name, :venmo_username, :total_spent, :expenses, :payments, :tallied_consumptions, :monthly_rent, :food_multiplier]
     
     # If using full display, comment line above and uncomment lines below
     # config.columns << :venmo_id
--- app/models/billing_period.rb
+++ app/models/billing_period.rb
@@ -155,7 +155,7 @@ class BillingPeriod < ActiveRecord::Base
       end
       payment.amount += total_for_person
       # not including details for this cause they're confusing - we don't actually know what's unaccounted
-      payment.details << {:category => "Unaccounted booze (very approximate)", :amount => total_for_person, :details => []}
+      payment.details << {:category => "Unaccounted booze + booze advance for next month", :amount => total_for_person, :details => []}
     end
   end
   
