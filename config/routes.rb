ea333d920853047fa48c2c9cc144f07b7878085f
Robert Cezar Matei <rmatei@gmail.com>
2011-05-17 18:14:14 -0700

    Putting navigation on every page and changing graphics

--- config/routes.rb
+++ config/routes.rb
@@ -65,7 +65,7 @@ Skyfarm::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "users#index"
 
   # See how all your routes lay out with "rake routes"
   
