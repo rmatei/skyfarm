35c29b9d2acfe89015468f25c9969304da15217b
Robert Cezar Matei <rmatei@gmail.com>
2011-05-17 18:28:11 -0700

    Adding booze types to view

--- app/controllers/tallied_items_controller.rb
+++ app/controllers/tallied_items_controller.rb
@@ -3,6 +3,9 @@ class TalliedItemsController < ApplicationController
   active_scaffold :tallied_items do |config|
     # Limited column display for usability
     config.columns = [:name, :price]
+
+    # Don't allow any changes to DB, just reading
+    config.actions = [:list, :show, :search]
   end
 
 end
