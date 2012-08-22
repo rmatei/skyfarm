9a6a740392c89047147364c6325446274244808b
Robert Cezar Matei <rmatei@gmail.com>
2012-08-22 15:20:26 -0700

    Adding fix for malformed JSON from Venmo

--- app/controllers/venmo_controller.rb
+++ app/controllers/venmo_controller.rb
@@ -41,6 +41,7 @@ private
     Rails.logger.info "Got params: #{params.inspect}"
     payments = params['payments'].split('.')[1]
     payments = Base64.decode64(payments)
+    payments += ']' unless payments[-1,1] == ']'
     payments = JSON.parse(payments)
     Rails.logger.info "Got payments from API: #{payments.inspect}"
     return payments
